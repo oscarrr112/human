c9c9dc11aed15b658ea64f202f4038b93bdd8786
离猝死不远队 <1261223497@qq.com>
2020-05-14 19:54:18 +0800

    修复了之前的一些bug

---
+++ xuanke/xuanke.js
@@ -0,0 +1,33 @@
+/*
+    请在下面结构中填入课程号(kch)以及你所想选的课序号(kxh)并复制进入course中 (别忘了回大括号后要加中文逗号)
+    {
+        kch:
+        kxh:
+    },
+    例如
+    let course = [
+        {
+            kch: 123,
+            kxh: 1
+        },
+        {
+            kch: 12,
+            kxh: 2
+        }
+    ];
+];
+*/
+
+let course = [
+    {
+        kch: 123,
+        kxh: 1
+    },
+    {
+        kch: 12,
+        kxh: 2
+    }
+];
+
+let input = document.getElementsByName('searchtj');
+let click = document.getElementById('queryButton');
